0f93fad72a125ea1342fc4a5298186d2c7f10c17
Blizonaire <Necesku@gmail.com>
2024-03-01 19:19:51 +0100

    Update

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { userRouter } from "./managers/user.js";
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { PrismaClient } from '@prisma/client';
+import fs from "fs";
 
 export const prisma = new PrismaClient()
 
@@ -24,11 +25,20 @@ app.use(userRouter);
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     log.error(err.stack, "Error Handler");
     res.status(500).send('An error occured while trying to handle your request.');
-  })
+});
 
 app.get("/", checkAuth, (req, res) => {
     res.render("dashboard", { "panel_title": config.panel_title, "token": "" });
 })
+
+app.get("/tab/:tab", checkAuth, (req, res) => {
+    const { tab } = req.params;
+    if (tab && fs.existsSync(path.join(__dirname, `/render/tabs/${tab}.ejs`))) {
+        res.render(`tabs/${tab.toString()}`);
+    } else {
+        res.render("dashboard", { "panel_title": config.panel_title, "token": "" });
+    }
+});
 
 app.listen(config.port, () => {
     log.success(`Listening on port ${config.port}.`, "app.ts")
